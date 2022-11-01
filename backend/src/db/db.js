b215bba4886e6595aef31b37ddeafe0de1d79921
drbobby27 <envioshseq@gmail.com>
2022-11-01 15:07:51 -0500

    Define basic express structure

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv/config'
 import { database, username, password, host, dialect} from '../helpers/helper.js'
 
 
-export const db = new Sequelize('', "delivery", "delivery123", {
+export const db = new Sequelize('delivery', "delivery", "delivery123", {
     host:'database-1.caidmnar18ph.us-east-1.rds.amazonaws.com',
     dialect:'mysql',
   })
